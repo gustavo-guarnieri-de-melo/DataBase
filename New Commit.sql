6e78aef1bf40d4e697e8c9738adcb642912ad6df
gustavo-guarnieri-de-melo <gustavoguarnieridemelo@gmail.com>
2023-03-29 19:03:44 -0300

    novos dados

---
+++ New Commit.sql
@@ -0,0 +1,13 @@
+insert into usuario values ('1','Gustavo','gustavoguarnieridemelo@gmail.com','123456');
+insert into usuario values ('2','Vinicius','vinicius@gmail.com','123456');
+insert into usuario values ('3','Marcel','marcel@gmail.com','123456');
+insert into usuario values ('4','Willian','will@gmail.com','123456');
+insert into usuario values ('5','Matheus','matheu@gmail.com','123456');
+
+insert into save1 values ('6','04-may-1997','caio','15');
+insert into save1 values ('7','02-may-2023','luiz','16');
+insert into save1 values ('8','03-may-2023','jose','17');
+insert into save1 values ('9','04-may-2023','marcelo','18');
+insert into save1 values ('10','10-may-2023','gustavo','19');
+
+
